3749c1e61af362458e88222d080dbf894c05b5d8
kabin <kabin@DESKTOP-A928918>
2024-04-24 01:48:49 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("883deb39-366c-4955-abf5-58d3f773e8a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ClassLibraryTests
+    {
+        [TestMethod]
+        public void TestDiagonalProduct_WithPositiveDiagonals_ReturnsExpectedResult()
+        {
+            double[,] values = new double[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+            ClassLibrary1 matrix = new ClassLibrary1(values);
+            int[] diagonals = new int[] { 0, 1 };
+
+            double result = matrix.Diagonal(diagonals);
+
+            Assert.AreEqual(540, result);
+        }
+
+        [TestMethod]
+        public void TestDiagonalProduct_WithNegativeDiagonals_ReturnsExpectedResult()
+        {
+            double[,] values = new double[,]
+            {
+                { -1, -2, -3 },
+                { -4, -5, -6 },
+                { -7, -8, -9 }
+            };
+            ClassLibrary1 matrix = new ClassLibrary1(values);
+            int[] diagonals = new int[] { -1, -2 };
+
+            double result = matrix.Diagonal(diagonals);
+
+            Assert.AreEqual(-224, result);
+        }
+
+        [TestMethod]
+        public void TestDiagonalProduct_WithZeroDiagonals_ReturnsExpectedResult()
+        {
+            double[,] values = new double[,]
+            {
+                { 0, 1, 2 },
+                { 3, 0, 4 },
+                { 5, 6, 0 }
+            };
+            ClassLibrary1 matrix = new ClassLibrary1(values);
+            int[] diagonals = new int[] { 0, -1 };
+
+            double result = matrix.Diagonal(diagonals);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
